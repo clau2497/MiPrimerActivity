6a16b7592c1242e7bee188f53b148c4c236dbb3c
Claudia Barajas <claudiabarajas76@gmail.com>
2023-06-17 10:03:38 -0600

    Primer Commit

---
+++ app/src/main/java/com/example/miprimeractivity/MyFirstActivity.kt
@@ -0,0 +1,12 @@
+package com.example.miprimeractivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyFirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    
+}
